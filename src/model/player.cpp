ae23dc33877f0cf75852657d3a719bd7ee3c52ae
javiberd <javierberdeciot@gmail.com>
2018-12-20 20:21:34 +0100

    Implemented Player::move method.

--- src/model/player.cpp
+++ src/model/player.cpp
@@ -1,23 +1,42 @@
 #include "player.h"
+#include "../common/common.h"
 
-void move(Direction direction) {
+void Player::move(Direction direction) {
+	int xTemp = x;
+	int yTemp = y;
 	switch(direction){
 	case North:
+		xTemp--;
 		break;
 	case NorthEast:
+		xTemp--;
+		yTemp++;
 		break;
 	case East:
+		yTemp++;
 		break;
 	case SouthEast:
+		xTemp++;
+		yTemp--;
 		break;
 	case South:
+		xTemp++;
 		break;
 	case SouthWest:
+		xTemp++;
+		yTemp--;
 		break;
 	case West:
+		yTemp--;
 		break;
 	case NorthWest:
+		xTemp--;
+		yTemp--;
 		break;
+	}
+	if (inMatrix(xTemp, yTemp, length_board)) {
+		x = xTemp;
+		y = yTemp;
 	}
 	return;
 }
