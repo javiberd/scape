5a997975d1704d1571752d1776d153caff5609a4
javiberd <javierberdeciot@gmail.com>
2018-12-20 21:02:23 +0100

    Added two functions.
    One function returns a random number, and the other returns a random
    bool.

--- src/common/common.cpp
+++ src/common/common.cpp
@@ -1,5 +1,15 @@
 #include "common.h"
+#include <cstdlib>
+#include <ctime>
 
 bool inMatrix(int x, int y, int n) {
 	return x >= 0 && y >= 0 && x < n && y < n;
+}
+
+int randomNumber(int a, int b) {
+	return a + (std::rand() % (b - a));
+}
+
+bool randomBool() {
+	return randomNumber(0, 2) == 1 ? true : false;
 }
--- src/common/common.h
+++ src/common/common.h
@@ -1,2 +1,10 @@
+#ifndef SRC_COMMON_COMMON_H_
+#define SRC_COMMON_COMMON_H_
 
 bool inMatrix(int x, int y, int n);
+
+int randomNumber(int a, int b);
+
+bool randomBool();
+
+#endif /* SRC_COMMON_COMMON_H_ */
