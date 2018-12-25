f94eb433381b0604de7952f2572e937eb740f4ad
javiberd <javierberdeciot@gmail.com>
2018-12-25 22:57:37 +0100

    Logic of the game implemented.

--- src/model/world.cpp
+++ src/model/world.cpp
@@ -5,16 +5,22 @@ World::World(const World &world) {
 	this->player = world.player;
 }
 
-World::World(Player &player, int length_board) : player(&player) {
-
+World::World(std::shared_ptr<Player> playerIn, int length_board) : player(playerIn) {
+	board = Matrix(length_board, length_board);
 }
 
 void World::movePlayer(Direction direction) {
-
+	(*player).move(board, direction);
 }
 
 void World::moveEntities() {
+	for (Matrix::iterator1 it = board.begin1(); it != board.end1(); ++it) {
+		(*it).get()->move(board);
+	}
+}
 
+bool World::gameOver() {
+	return board((*player).getX(), (*player).getY()) != nullptr;
 }
 
 void World::notifyAll() {
--- src/model/world.h
+++ src/model/world.h
@@ -3,19 +3,33 @@
 
 #include "observable.h"
 #include "player.h"
+#include "entity.h"
+
+typedef boost::numeric::ublas::matrix<std::shared_ptr<Entity> > Matrix;
 
 class World: public Observable {
+
 private:
 	std::shared_ptr<Player> player;
-	boost::numeric::ublas::matrix<std::shared_ptr<Entity> > board;
+	Matrix board;
+
 public:
 	World() {};
+
 	World(const World& world);
-	World(Player &player, int length_board);
+
+	World(std::shared_ptr<Player> player, int length_board);
+
 	void movePlayer(Direction);
+
 	void moveEntities();
+
+	bool gameOver();
+
 	void notifyAll();
+
 	void addObserver();
+
 };
 
 #endif /* SCAPE_SRC_MODEL_WORLD_H_ */
