8da2e1c846f504d4e11f024cafeac0a0bb8f4a28
javiberd <javierberdeciot@gmail.com>
2018-12-26 20:13:38 +0100

    Board drawing done.
    This is a first implementation of this.

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -20,6 +20,32 @@ Gui::Gui() {
 
 
 void Gui::start() {
+//
+//	nana::drawing dw(form);
+
+//	dw.draw([](nana::paint::graphics& graph) {
+//		graph.rectangle(nana::rectangle{10, 10, 15, 30}, true, nana::colors::red);
+//	});
+
+//	dw.update();
+
+//	int matrix[5][5] = {
+//			0, 1, 0, 1, 0,
+//			1, 1, 1, 0, 1,
+//			0, 1, 0, 1, 0,
+//			1, 1, 1, 0, 1,
+//			0, 1, 0, 1, 0};
+//
+//	for (unsigned int i = 0; i < 5; i++) {
+//			for (unsigned int j = 0; j < 5; j++) {
+//				if (matrix[i][j] == 1) {
+//					dw.draw([i, j](nana::paint::graphics& graph) {
+//						graph.rectangle(nana::rectangle{50 * i, 50 * j, 49, 49}, true, nana::colors::red);
+//					});
+//				}
+//			}
+//		}
+
 	//Show the form
 	form.show();
 
@@ -28,5 +54,23 @@ void Gui::start() {
 }
 
 void Gui::update(const std::shared_ptr<Player> player, const Matrix &board) {
+	nana::drawing dw(form);
 
+	// Draw player
+	int x = (*player).getX();
+	int y = (*player).getY();
+	dw.draw([x, y](nana::paint::graphics& graph) {
+		graph.rectangle(nana::rectangle{50 * x, 50 * y, 49, 49}, true, nana::colors::blue);
+	});
+
+	// Draw board
+	for (unsigned int i = 0; i < board.size1(); i++) {
+		for (unsigned int j = 0; j < board.size2(); j++) {
+			if (board(i,j) != nullptr) {
+				dw.draw([i, j](nana::paint::graphics& graph) {
+					graph.rectangle(nana::rectangle{50 * i, 50 * j, 49, 49}, true, nana::colors::red);
+				});
+			}
+		}
+	}
 }
