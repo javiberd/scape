cec5dc8f8a7fd5eb510f404c2443dda4bb626271
javiberd <javierberdeciot@gmail.com>
2018-12-26 20:20:49 +0100

    Deleted irrelevant includes.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,10 +2,6 @@
 #include "../controller/controller.h"
 #include "../model/world.h"
 
-#include <nana/gui.hpp>
-#include <nana/gui/widgets/label.hpp>
-#include <nana/gui/widgets/button.hpp>
-
 int main() {
 	Gui gui;
 	std::srand(std::time(nullptr));
