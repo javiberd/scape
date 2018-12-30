764fef0b1bcbeff4d0baab2e46d841a5f58ecf10
javiberd <javierberdeciot@gmail.com>
2018-12-30 23:13:56 +0100

    Done initialization of objects in main.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3,7 +3,10 @@
 #include "../model/world.h"
 
 int main() {
-	Gui gui;
 	std::srand(std::time(nullptr));
+	World world;
+	Controller controller(world);
+	Gui gui(controller);
+	world.addObserver((std::shared_ptr<Observer>) &gui);
 	gui.start();
 }
