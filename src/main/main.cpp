0a39d0ddd1dc8fcd71803dc225a2d13e7bd394d9
javiberd <javierberdeciot@gmail.com>
2019-05-25 10:37:55 +0200

    Adapted initilization ot changes in the main objects used.
    Partially functioning.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3,10 +3,14 @@
 #include "../model/world.h"
 
 int main() {
+	// Sets the seed of the random number generator
 	std::srand(std::time(nullptr));
+	// Initialization of main objects
 	World world;
 	Controller controller(world);
 	Gui gui(controller);
-	world.addObserver((std::shared_ptr<Observer>) &gui);
-	gui.start();
+	world.addObserver(std::make_shared<Gui>(gui));
+	// This is for updating the graphic view with the
+	// starting board
+	world.notifyAll();
 }
