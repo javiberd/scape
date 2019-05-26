eaca1e56ff57c74fbdf2ec80aedc96512c5d65d2
javiberd <javierberdeciot@gmail.com>
2019-05-26 21:01:53 +0200

    Added two new functions for starting and restarting the game.
    Modified step function in terms of the changes in World class.

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -4,7 +4,14 @@ Controller::Controller (World &world) : world(world) {
 
 }
 
+void Controller::start() {
+	world.notifyAll();
+}
+
+void Controller::restart() {
+	world.restart();
+}
+
 void Controller::step(Direction direction) {
-	world.movePlayer(direction);
-	world.moveEntities();
+	world.step(direction);
 }
