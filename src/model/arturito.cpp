11c81ffbc696946bf65df986b1b4c8f898c1c65e
javiberd <javierberdeciot@gmail.com>
2019-05-26 21:00:07 +0200

    Reformated.

--- src/model/arturito.cpp
+++ src/model/arturito.cpp
@@ -1,18 +1,20 @@
 #include "arturito.h"
 #include "../common/common.h"
 
-Arturito::Arturito(int xIn, int yIn) : Entity(xIn, yIn) {
+Arturito::Arturito(int xIn, int yIn) :
+		Entity(xIn, yIn) {
 
 }
 
-void Arturito::move(boost::numeric::ublas::matrix<std::shared_ptr<Entity> > &board) {
+void Arturito::move(
+		boost::numeric::ublas::matrix<std::shared_ptr<Entity> > &board) {
 	int xAdd, yAdd;
 	do {
 		xAdd = randomBool() ? 1 : -1;
 		yAdd = randomBool() ? 1 : -1;
 	} while (!inMatrix(x + xAdd, y + yAdd, board.size1()));
-	board (x + xAdd, y + yAdd) = board (x, y);
-	board (x, y) = nullptr;
+	board(x + xAdd, y + yAdd) = board(x, y);
+	board(x, y) = nullptr;
 	x = x + xAdd;
 	y = y + yAdd;
 }
